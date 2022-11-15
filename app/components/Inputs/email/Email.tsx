bb7eba144aa221375c58994978c202a345fb81b1
arthurkyambadde <arthurkyambadde9@gmail.com>
2022-11-15 22:03:41 +0300

    fix: email auth

--- app/components/Inputs/email/Email.tsx
+++ app/components/Inputs/email/Email.tsx
@@ -1,34 +1,17 @@
-import type { ForwardedRef } from "react";
-import React from "react";
-import styles from "./styles.css";
+import type { ForwardedRef } from 'react';
+import React from 'react';
+import styles from './styles.css';
+import { handleSubmit, emailValidator } from '../../../Utils/emailValidator';
 
-export const links = () => [{ rel: "stylesheet", href: styles }];
+export const links = () => [{ rel: 'stylesheet', href: styles }];
 
 type EmailInputProps = {
   children?: React.ReactNode;
   id?: string;
 };
 
-function emailValidator(e: React.FocusEvent<HTMLInputElement>) {
-  const pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
-  const inputElement: HTMLInputElement | null = e.target;
-  const emailValue = inputElement?.value;
-  const message: any = document.querySelector(".text-message");
-
-  if (!emailValue) {
-    message.innerHTML = "Can't be empty";
-    console.log("am doing something about Can't be empty");
-  } else if (!emailValue.match(pattern)) {
-    message.innerHTML = "Invalid Email Address";
-    console.log("am doing something about Invalid Email Address");
-  }
-}
-
 export const EmailInput = React.forwardRef(
-  (
-    { children, ...props }: EmailInputProps,
-    ref: ForwardedRef<HTMLInputElement>
-  ) => {
+  ({ children, ...props }: EmailInputProps, ref: ForwardedRef<HTMLInputElement>) => {
     return (
       <label className="email_container" htmlFor={props.id}>
         <input
@@ -39,10 +22,13 @@ export const EmailInput = React.forwardRef(
           {...props}
           placeholder="Email address"
           required
-          onBlur={emailValidator}
+          onFocus={emailValidator}
+          onKeyDown={(e) => {
+            handleSubmit(e);
+          }}
         />
         <p className="text-message"></p>
       </label>
     );
-  }
+  },
 );
