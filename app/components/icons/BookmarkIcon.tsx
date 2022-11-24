3271253a3608b7d4660b7ecdd1b75498e637b75e
Ssejjemba Dan <ssejjembadan@gmail.com>
2022-11-24 21:55:42 +0300

    (styles): Linked styles for the bookmark icon

--- app/components/icons/BookmarkIcon.tsx
+++ app/components/icons/BookmarkIcon.tsx
@@ -13,18 +13,19 @@ export const BookmarkIcon = ({ active }: BookmarkIconProps) => {
     className = "active";
   }
   return (
-    <svg
-      width="12"
-      height="14"
-      xmlns="http://www.w3.org/2000/svg"
-      className={`icon ${className}`}
-      data-testid="bookmark-icon"
-    >
-      <path
-        d="m10.518.75.399 12.214-5.084-4.24-4.535 4.426L.75 1.036l9.768-.285Z"
-        strokeWidth="1.5"
-        fill="none"
-      />
-    </svg>
+    <div className={`container ${className}`}>
+      <svg
+        width="12"
+        height="14"
+        xmlns="http://www.w3.org/2000/svg"
+        className="icon"
+        data-testid="bookmark-icon"
+      >
+        <path
+          d="m10.518.75.399 12.214-5.084-4.24-4.535 4.426L.75 1.036l9.768-.285Z"
+          strokeWidth="1.5"
+        />
+      </svg>
+    </div>
   );
 };
--- app/pages/home/thumbnail/Thumbnail.tsx
+++ app/pages/home/thumbnail/Thumbnail.tsx
@@ -1,9 +1,16 @@
 import React, { useState } from "react";
-import { IdleIcon } from "../../../components/bookmark-icons/idle-icon/IdleIcon";
-import { ActiveIcon } from "~/components/bookmark-icons/active-icon/ActiveIcon";
+// import { IdleIcon } from "../../../components/bookmark-icons/idle-icon/IdleIcon";
+// import { ActiveIcon } from "~/components/bookmark-icons/active-icon/ActiveIcon";
+import {
+  BookmarkIcon,
+  links as BookmarkStyles,
+} from "~/components/icons/BookmarkIcon";
 import styles from "./styles.css";
 
-export const links = () => [{ rel: "stylesheet", href: styles }];
+export const links = () => [
+  { rel: "stylesheet", href: styles },
+  ...BookmarkStyles(),
+];
 
 type ThumbnailProps = {
   children?: React.ReactNode;
@@ -12,21 +19,14 @@ type ThumbnailProps = {
 };
 
 export const Thumbnail = ({ name, src }: ThumbnailProps) => {
-  const [bookmarked, setbookmarked] = useState(false);
+  const [isBookmarked, setIsBookmarked] = useState(false);
   return (
     <div
       className="thumbnail-container"
       data-testid="thumbnail"
-      onClick={() => setbookmarked(true)}
+      onClick={() => setIsBookmarked(true)}
     >
-      {bookmarked ? (
-        <ActiveIcon
-          className="bookmark-icon active-bookmark"
-          id="active-bookmark"
-        />
-      ) : (
-        <IdleIcon className="bookmark-icon idle-bookmark" id="idle-bookmark" />
-      )}
+      <BookmarkIcon active={isBookmarked} />
       <img
         alt={name}
         src={src}
