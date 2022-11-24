d625dfb1f2ba15e49ab7f0d7a3c0216ad2f06119
arthurkyambadde <arthurkyambadde9@gmail.com>
2022-11-24 10:44:55 +0300

    feat: add bookmark component

--- app/components/bookmark/bookmark.tsx
+++ app/components/bookmark/bookmark.tsx
@@ -9,28 +9,37 @@ export const links = () => [{ rel: "stylesheet", href: styles }];
 type bookmarkProps = {
     image : string
 }
-
-
-
-
-const [first, setfirst] = useState("idle")
-
-// const handleBookmarkHover = ()=>{
-//   setCardState("hover")
-// }
-
-// const handleBookmarkClick =()=>{
+    
+    export const   Bookmark=(props:bookmarkProps) =>{
+      const {image} = props 
   
-// }
+      const [bookmarkState, setBookmarkState] = useState("idle")
 
-export const   Bookmark=(props:bookmarkProps) =>{
-  const {image} = props 
+      const handleBookmarkMouseEnter = ()=>{
+        setBookmarkState("idle")
+        console.log(bookmarkState)
+      }
+      const handleBookmarkClick = ()=>{
+        if(bookmarkState === "hover"){
+          setBookmarkState("active")
+        }
+        
+        if(bookmarkState === "active"){
+          setBookmarkState("hover")
+        }
+        console.log(bookmarkState)
+      }
+      const handleBookmarkMouseLeave = ()=>{
+        setBookmarkState("hover")
+        console.log(bookmarkState)
+      }
+
 
 //   console.log(cardState)
   
     return (
     <div data-testid="bookmark" className='bookmark_container'>
-        <img src={image} alt="bookmark" className='bookmark_image' />
+        <img src={image} alt="bookmark" className='bookmark_image' onMouseEnter={handleBookmarkMouseEnter} onMouseLeave={handleBookmarkMouseLeave} onClick={handleBookmarkClick} />
         <img src={idleBookmarkicon} alt="" className='bookmark_icon' />
 
     </div>
