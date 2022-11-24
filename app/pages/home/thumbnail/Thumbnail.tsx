64f617a022f7e9e0710e20b40c0467398a3b2957
Ssejjemba Dan <ssejjembadan@gmail.com>
2022-11-25 00:38:10 +0300

    (Refactor, styles): Update styles to show thumbnail states

--- app/pages/home/thumbnail/Thumbnail.tsx
+++ app/pages/home/thumbnail/Thumbnail.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
-// import { IdleIcon } from "../../../components/bookmark-icons/idle-icon/IdleIcon";
-// import { ActiveIcon } from "~/components/bookmark-icons/active-icon/ActiveIcon";
+import {
+  PlayButton,
+  links as PlayButtonStyles,
+} from "~/components/buttons/PlayButton";
 import {
   BookmarkIcon,
   links as BookmarkStyles,
@@ -10,6 +12,7 @@ import styles from "./styles.css";
 export const links = () => [
   { rel: "stylesheet", href: styles },
   ...BookmarkStyles(),
+  ...PlayButtonStyles(),
 ];
 
 type ThumbnailProps = {
@@ -20,30 +23,22 @@ type ThumbnailProps = {
 
 export const Thumbnail = ({ name, src }: ThumbnailProps) => {
   const [isBookmarked, setIsBookmarked] = useState(false);
+
   return (
     <div
       className="thumbnail-container"
       data-testid="thumbnail"
-      onClick={() => setIsBookmarked(true)}
+      onClick={() => setIsBookmarked(!isBookmarked)}
+      style={{
+        background: `url(${src})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+      role="button"
     >
       <BookmarkIcon active={isBookmarked} />
-      <img
-        alt={name}
-        src={src}
-        className="thumbnail-img"
-        data-testid="thumbnail-img"
-      />
-      <div className="controls-container">
-        <div className="play-control" data-testid="play-control">
-          <svg width="30" height="30" xmlns="http://www.w3.org/2000/svg">
-            <path
-              d="M15 0C6.713 0 0 6.713 0 15c0 8.288 6.713 15 15 15 8.288 0 15-6.712 15-15 0-8.287-6.712-15-15-15Zm-3 21V8l9 6.5-9 6.5Z"
-              fill="#fff"
-            />
-          </svg>
-          <span className="extra_small_heading">Play</span>
-        </div>
-      </div>
+      <PlayButton className="invisible_element" />
+      <div className="overlay"></div>
     </div>
   );
 };
