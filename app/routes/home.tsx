07a8c85a7865a7b1c7b673f95020cca844597fc2
DESKTOP-G60BJ3E\Ssonko Jimmy <ssembatyadennis25@gmail.com>
2022-12-01 11:02:13 +0300

    feat(Search Component): add a Search component to the home route.

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,11 +1,16 @@
 import { Nav, links as NavigationStyles } from "~/components/nav/Navigation";
 import { Row, links as RowStyles } from "~/components/row/Row";
 import { Grid, links as gridStyles } from "~/components/grid/Grid";
+import {
+  SearchInput,
+  links as SearchInputStyles,
+} from "~/components/Inputs/search/Search";
 import styles from "../styles/home.css";
 
 export const links = () => [
   { rel: "stylesheet", href: styles },
   ...NavigationStyles(),
+  ...SearchInputStyles(),
   ...gridStyles(),
   ...RowStyles(),
 ];
@@ -15,6 +20,7 @@ export default function Home() {
     <div className="home_container">
       <Nav />
       <main>
+        <SearchInput />
         <Row />
         <Grid />
       </main>
