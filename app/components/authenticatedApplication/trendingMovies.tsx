e86663b66ece829593f6873ea08e7d0b17f64e45
arthurkyambadde <arthurkyambadde9@gmail.com>
2022-12-04 21:33:35 +0300

    feat: add trending movies styles

--- app/components/authenticatedApplication/trendingMovies.tsx
+++ app/components/authenticatedApplication/trendingMovies.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import { Thumbnail } from '../thumbnail/thumbnail';
 import thumbnailDot from '../../../public/assets/thumbnailDot.svg';
@@ -14,13 +14,40 @@ export const TrendingMovies = (props: trendingMovieProps) => {
 
   const trendingMovieData = data.filter((item) => item.isTrending);
 
+  useEffect(() => {
+    const slides = document.querySelectorAll(
+      '.trending',
+    ) as unknown as HTMLCollectionOf<HTMLElement>;
+
+    const slideCount = slides.length;
+    let i = 0;
+
+    function moveLeft() {
+      i++;
+      if (i < slideCount) {
+        slides[i].style.width = '0';
+        slides[i - 1].style.width = '-100%';
+      } else {
+        i = 0;
+        slides[i].style.width = '0';
+        slides[slideCount - 1].style.width = '-100%';
+
+        for (let x = 1; x < slideCount - 1; x++) {
+          slides[x].style.width = '100%';
+        }
+      }
+    }
+
+    setInterval(moveLeft, 3000);
+  }, []);
+
   return (
     <>
       <div>
         <p className="category_tag">Trending</p>
-        <div className="trending_movies--container">
+        <div className="trending_movies--container ">
           {trendingMovieData.map((item) => (
-            <div key={item.title} className="thumbnail_wrapper">
+            <div key={item.title} className="thumbnail_wrapper trending">
               <Thumbnail
                 medium={item.thumbnail?.regular?.medium}
                 large={item.thumbnail?.regular?.large}
