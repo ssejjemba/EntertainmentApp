5a4b46e2c86c1ce946495e3a2628864a3f434216
DESKTOP-G60BJ3E\Ssonko Jimmy <ssembatyadennis25@gmail.com>
2022-12-14 13:51:09 +0300

    feat(home page): change banner and thumbnails being rendered with click on different icons.
    - start with setting state in the App and passing it down as props to all children components.

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Nav, links as NavigationStyles } from "~/components/nav/Navigation";
 import { Row, links as RowStyles } from "~/components/row/Row";
 import { Grid, links as gridStyles } from "~/components/grid/Grid";
@@ -16,6 +17,7 @@ export const links = () => [
 ];
 
 export default function Home() {
+  const [movieCategory, setMovieCategory] = useState("home");
   return (
     <div className="home_container">
       <Nav />
