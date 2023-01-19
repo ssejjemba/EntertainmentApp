85313efa7e02f34589b3316a5a4a707b1d5e7aea
DESKTOP-G60BJ3E\Ssonko Jimmy <ssembatyadennis25@gmail.com>
2023-01-19 22:59:39 +0300

    (buisness logic): Add store state and functions

--- app/store/data.ts
+++ app/store/data.ts
@@ -27,15 +27,67 @@ interface Movie {
   isTrending: boolean;
 }
 
-interface MovieStore {
-  data: Movie[];
-  getBookmarkedMovies: () => Movie[];
-}
-export const useMovieDataStore = create<MovieStore>((set) => {
+type State = {
+  currentData: Movie[];
+  activeFilter: string;
+  activeFilterText: string;
+  bookmarkedMovies: Movie[];
+  tvShows: Movie[];
+  movies: Movie[];
+  textFilteredData: Movie[];
+};
+
+type Actions = {
+  setActiveFilter: (filter: string) => void;
+  setActiveFilterText: (text: string) => void;
+};
+
+export const useMovieDataStore = create<State & Actions>((set) => {
   return {
-    data,
-    getBookmarkedMovies: () => {
+    activeFilter: "text",
+    activeFilterText: "",
+    setActiveFilter: (filter: string) =>
+      set((state) => ({ activeFilter: filter })),
+    setActiveFilterText: (text: string) =>
+      set((state) => ({ activeFilterText: text })),
+    get bookmarkedMovies() {
       return data.filter((movie) => movie.isBookmarked === true);
+    },
+    get tvShows() {
+      return data.filter((movie) => movie.category === "TV Series");
+    },
+    get movies() {
+      return data.filter((movie) => movie.category === "Movie");
+    },
+    get textFilteredData() {
+      if (!this.activeFilterText) {
+        return data;
+      }
+      return data.filter((movie) =>
+        movie.title.includes(this.activeFilterText)
+      );
+    },
+    toggleBookmark(title: string) {
+      const movie = data.find((movie) => movie.title === title);
+      if (movie) {
+        movie.isBookmarked = !movie.isBookmarked;
+      }
+    },
+
+    get currentData() {
+      if (this.activeFilter === "bookmarks") {
+        return this.bookmarkedMovies;
+      }
+
+      if (this.activeFilter === "tvshows") {
+        return this.tvShows;
+      }
+
+      if (this.activeFilter === "movies") {
+        return this.movies;
+      }
+
+      return this.textFilteredData;
     },
   };
 });
